e8c5d772fb94a75655d1f64f8d0f2be79cd8dcd4
Naveen Ravi <gamerkidnave@gmail.com>
2023-01-04 23:44:00 +0530

    bug fixes

--- src/Controllers/Retrive/Retrive.helper.js
+++ src/Controllers/Retrive/Retrive.helper.js
@@ -8,9 +8,10 @@ try{
     return new Promise(async(resolve,reject)=>{
         if(!fs.existsSync(path.join(imagefolder,"\\",query.collection))) reject({message:'Collection Does Not Exist'});
         if(fs.existsSync(path.join(imagefolder,"\\",query.collection,"\\",query.url))){
-        let imglog=await imageLogModel.findOne({userid:query.userid})
+        let imglog=await imageLogModel.find({userid:query.userid})
+        imglog= imglog.find((log)=>log.filename==query.url)
         if(imglog==null||imglog==undefined) return reject({message:"Access Denied"})
-        if(imglog.filename!=query.url) reject({message:'Access Denied'})
+        if(imglog.filename!==query.url) reject({message:'Access Denied'})
         resolve(fs.readFileSync(path.join(imagefolder,"\\",query.collection,"\\",query.url)))
         } else reject({message:"Item with the specified url doesnt exist"});
         })
