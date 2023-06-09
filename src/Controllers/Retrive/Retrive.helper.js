b5e4cda348dcae5abb017d2cd49f61345b85b196
Naveenravi07 <gamerkidnav@gmail.com>
2023-06-09 20:50:19 +0530

    Updated Url Issue In Linux

--- src/Controllers/Retrive/Retrive.helper.js
+++ src/Controllers/Retrive/Retrive.helper.js
@@ -6,14 +6,14 @@ const { imageLogModel } = require('../../Models/Images/Imagelog.model');
 const RetriveImageFromServer = (query) => {
 try{
     return new Promise(async(resolve,reject)=>{
-        if(!fs.existsSync(path.join(imagefolder,"\\",query.collection))) reject({message:'Collection Does Not Exist'});
-        if(fs.existsSync(path.join(imagefolder,"\\",query.collection,"\\",query.url))){
+        if(!fs.existsSync(path.join(imagefolder,"/",query.collection))) reject({message:'Collection Does Not Exist'});
+        if(fs.existsSync(path.join(imagefolder,"/",query.collection,"/",query.url))){
         let imglog=await imageLogModel.find({userid:query.userid})
         imglog= imglog.find((log)=>log.filename==query.url)
         if(imglog==null||imglog==undefined) return reject({message:"Access Denied"})
         if(imglog.filename!==query.url) reject({message:'Access Denied'})
         if(imglog.userid!==query.userid) reject({message:'Access Denied'})
-        resolve(fs.readFileSync(path.join(imagefolder,"\\",query.collection,"\\",query.url)))
+        resolve(fs.readFileSync(path.join(imagefolder,"/",query.collection,"/",query.url)))
         } else reject({message:"Item with the specified url doesnt exist"});
         })
 }catch(err){throw {message:"An Unknown Error Occured"}}
