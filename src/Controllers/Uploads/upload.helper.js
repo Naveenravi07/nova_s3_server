2c587f21bad47e43a360960fa05ffc1bd60e9372
Naveenravi07 <gamerkidnav@gmail.com>
2023-06-20 23:25:19 +0530

    bug FIx

--- src/Controllers/Uploads/upload.helper.js
+++ src/Controllers/Uploads/upload.helper.js
@@ -27,6 +27,7 @@ const uploadProfileHelper = (req, res) => {
     return new Promise(async (resolve, reject) => {
         uploadimg(req, res, (multererr) => {
             if (multererr) reject(multererr)  
+            else{
             req.file.path=req.file.path.split("s3bucket").pop()
             // Add the user id after jwt 
              new imageLogModel({filename:req.file.filename,path:req.file.path,orginalfilename:req.file.originalname,size:req.file.size,mimetype:req.file.mimetype,userid:req.body.userid}).save()
@@ -35,6 +36,7 @@ const uploadProfileHelper = (req, res) => {
             delete req.file.encoding
             req.file.url=`${ApiEndpoints.s3imageRetriveurl}?collection=${req.query.collection}&userid=${req.body.userid}&url=${req.file.filename}`
             resolve({status:req.file})
+            }
         })
     })
 }
